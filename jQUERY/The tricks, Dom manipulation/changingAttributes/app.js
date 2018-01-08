cc193645fce4a3093c1b946c47c0abda2ad4542b
gbengaoti <gbengaoti@rocktmail.com>
2018-01-08 21:01:11 +0100

    Add jquery

---
+++ jQUERY/The tricks, Dom manipulation/changingAttributes/app.js
@@ -0,0 +1,15 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+var navList;
+
+navList = $(".nav-list").children().first();
+
+link = navList.find("a");
+
+link.attr("href", "#1");
+console.log(navList);
